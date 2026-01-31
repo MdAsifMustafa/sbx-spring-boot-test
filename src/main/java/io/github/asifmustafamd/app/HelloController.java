bd74ce443618185a36cb51758a5b810a6fcd475a
Asim187 <asim40906@gmail.com>
2026-01-31 20:30:31 +0530

    Use properties-based dev, prod, and test profiles

--- src/main/java/io/github/asifmustafamd/app/HelloController.java
+++ src/main/java/io/github/asifmustafamd/app/HelloController.java
@@ -1,13 +1,17 @@
 package io.github.asifmustafamd.app;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+	
+	 @Value("${spring.profiles.active:default}")
+	    private String profile;
 
     @GetMapping("/hello")
     public String hello() {
-        return "hello";
+    	  return "hello (" + profile + ")";
     }
 }
