de6ad93ac45d48cfa107659682881bbb42ca069f
Asim187 <asim40906@gmail.com>
2026-01-31 20:02:31 +0530

    Initial Spring Boot hello app

---
+++ src/main/java/io/github/asifmustafamd/app/SbxSpringBootTestApplication.java
@@ -0,0 +1,13 @@
+package io.github.asifmustafamd.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbxSpringBootTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbxSpringBootTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/asifmustafamd/app/SbxSpringBootTestApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.asifmustafamd.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbxSpringBootTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
